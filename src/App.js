daf9d4c6805d8b18e3500abf05fae2105ceaf78d
Ben G <bensgar92@gmail.com>
2021-01-23 15:10:16 -0700

    started on start menu functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Main from "./pages/main/main"
-import Footer from "./components/footer/footer"
+// import Footer from "./components/footer/footer"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './app.css'
 
@@ -11,7 +11,7 @@ function App() {
       <div className="background">
         <Route exact path="/" component={Main} />
         <Route exact path="/main" component={Main} />
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </Router>
   );
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,20 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Footer from '../../components/footer/footer'
 import DragIcon1 from '../../components/icon/drag1'
 import DragIcon2 from '../../components/icon/drag2'
 import DragIcon3 from '../../components/icon/drag3'
 import DragIcon4 from '../../components/icon/drag4'
 import DragIcon5 from '../../components/icon/drag5'
 import DragIcon6 from '../../components/icon/drag6'
+import './main.css'
 
 function Main() {
+
+    const [showStart, setShowStart] = useState(false)
+
+    const openStart = () => {
+        setShowStart(true)
+        console.log("Test")
+    }
+
+    const closeStart = () => {
+        setShowStart(false)
+        console.log("Test2")
+    }
+
         return (
-            <div className="background">
-                <DragIcon1 />
-                <DragIcon2 />
-                <DragIcon3 />
-                <DragIcon4 />
-                <DragIcon5 />
-                <DragIcon6 />
+            <div>
+                <div className="background">
+                    <DragIcon1 />
+                    <DragIcon2 />
+                    <DragIcon3 />
+                    <DragIcon4 />
+                    <DragIcon5 />
+                    <DragIcon6 />
+                </div>
+                { showStart ? 
+                    <div className="startBox">
+                        <div className="row">
+                            <div className="vertical col-sm-2">
+                                Windows 95
+                            </div>
+                            <div className="startMenuButtons col-sm-10">
+                                <div className="col-sm">
+                                    <button>BUTTON</button>
+                                </div>
+                                <div className="col-sm">
+                                    <button>BUTTON</button>
+                                </div>
+                                <div className="col-sm">
+                                    <button>BUTTON</button>
+                                </div>
+                                <div className="col-sm">
+                                    <button>BUTTON</button>
+                                </div>
+                                <div className="col-sm">
+                                    <button>BUTTON</button>
+                                </div>
+                                <div className="col-sm">
+                                    <button>BUTTON</button>
+                                </div>
+                                <div className="col-sm">
+                                    <button>BUTTON</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                : null}
+                <Footer start={openStart} closeStart={closeStart}/>
             </div>
         )
 }
