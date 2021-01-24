9b70cf5fb95337b11226196beec14ce8006b6fd6
Ben G <bensgar92@gmail.com>
2021-01-23 17:55:14 -0700

    perfecting the start menu

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -34,31 +34,31 @@ function Main() {
                 </div>
                 { showStart ? 
                     <div className="startBox">
-                        <div className="row">
-                            <div className="vertical col-sm-2">
-                                Windows 95
+                        <div className="row windows95Row">
+                            <div className="vertical col-sm-3">
+                                <span className="spanWindows">Windows</span> <span className="span95">95</span>
                             </div>
-                            <div className="startMenuButtons col-sm-10">
-                                <div className="col-sm">
-                                    <button>BUTTON</button>
+                            <div className="startMenuButtons col-sm-9 p-0">
+                                <div className="col-sm startDiv">
+                                    <div className="divButton"><img src="https://daks2k3a4ib2z.cloudfront.net/57b0b52271bead8852aa6781/57b1c44254eef5b558d0147b_Programs32x32.png"/>Programs<img className="arrowRight" src="https://daks2k3a4ib2z.cloudfront.net/57b0b52271bead8852aa6781/57b124d371bead8852ab0e91_Menu%20Arrow.png"/></div>
                                 </div>
-                                <div className="col-sm">
-                                    <button>BUTTON</button>
+                                <div className="col-sm startDiv">
+                                    <div className="divButton"><img src="https://daks2k3a4ib2z.cloudfront.net/57b0b52271bead8852aa6781/57b0bd8e54eef5b558ce88a8_Documents%2032x32.png"/>Documents<img className="arrowRight" src="https://daks2k3a4ib2z.cloudfront.net/57b0b52271bead8852aa6781/57b124d371bead8852ab0e91_Menu%20Arrow.png"/></div>
                                 </div>
-                                <div className="col-sm">
-                                    <button>BUTTON</button>
+                                <div className="col-sm startDiv">
+                                    <div className="divButton"><img src="https://daks2k3a4ib2z.cloudfront.net/57b0b52271bead8852aa6781/57b0bd8ee18710ca16498bb6_Settings%2032x32.png"/>Settings<img className="arrowRight" src="https://daks2k3a4ib2z.cloudfront.net/57b0b52271bead8852aa6781/57b124d371bead8852ab0e91_Menu%20Arrow.png"/></div>
                                 </div>
-                                <div className="col-sm">
-                                    <button>BUTTON</button>
+                                <div className="col-sm startDiv">
+                                    <div className="divButton"><img src="https://daks2k3a4ib2z.cloudfront.net/57b0b52271bead8852aa6781/57b1c4421fd32dc352b2e255_Find%2032x32.png"/>Find<img className="arrowRight" src="https://daks2k3a4ib2z.cloudfront.net/57b0b52271bead8852aa6781/57b124d371bead8852ab0e91_Menu%20Arrow.png"/></div>
                                 </div>
-                                <div className="col-sm">
-                                    <button>BUTTON</button>
+                                <div className="col-sm startDiv">
+                                    <div className="divButton"><img src="https://daks2k3a4ib2z.cloudfront.net/57b0b52271bead8852aa6781/57b1c442ec62b90517b967cc_Help%2032x32.png"/>Help</div>
                                 </div>
-                                <div className="col-sm">
-                                    <button>BUTTON</button>
+                                <div className="col-sm startDiv">
+                                    <div className="divButton"><img src="https://daks2k3a4ib2z.cloudfront.net/57b0b52271bead8852aa6781/57b0bd8e54eef5b558ce88aa_Run%2032x32.png"/>Run</div>
                                 </div>
-                                <div className="col-sm">
-                                    <button>BUTTON</button>
+                                <div className="col-sm startDiv">
+                                    <div className="divButton divShutdown"><img src="https://daks2k3a4ib2z.cloudfront.net/57b0b52271bead8852aa6781/57b0bd8e71bead8852aa8020_Shut%20Down%2032x32.png"/>Shut Down...</div>
                                 </div>
                             </div>
                         </div>
