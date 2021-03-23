51f00bbf411477b5403df991b06a6284eb5d7a5e
Ben Gardner <B.Gardner-contractor@kyrio.com>
2021-03-23 15:05:12 -0600

    made minor clock adjustment

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -15,8 +15,8 @@ function Footer(props) {
         <div className="footerBackground">
             <footer className="page-footer font-small py-1 text-white-50 fixed-bottom">
             <button onClick={startButton} className="startButton"><img src="https://daks2k3a4ib2z.cloudfront.net/57b0b52271bead8852aa6781/57b11afbe18710ca164a57b1_Start%20Logo%2032x32.png"/>Start</button>
-                <div className="clock">
-                    <Clock />
+                <div className="clockBorder">
+                    <div className="clock"><Clock /></div>
                 </div>
             </footer>
         </div>
